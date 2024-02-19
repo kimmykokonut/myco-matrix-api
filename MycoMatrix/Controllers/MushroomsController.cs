17e82d4b50e06d9dad30a0248fb941b89ad9a514
Kim Robinson <kimmykokonut@gmail.com>
2024-02-19 11:07:47 -0800

    add put functionality for api
    Co-authored-by: Henry Oberholtzer <henryoberholtzer@gmail.com>

--- MycoMatrix/Controllers/MushroomsController.cs
+++ MycoMatrix/Controllers/MushroomsController.cs
@@ -32,6 +32,61 @@ namespace MycoMatrix.Controllers
       }
       return m;
     }
+    // POST api/mushrooms
+    [HttpPost]
+    public async Task<ActionResult<Mushroom>> Post(Mushroom m)
+    {
+      _db.Mushrooms.Add(m);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetMushroom), new { id = m.MushroomId }, m);
+    }
+
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteMushroom(int id)
+    {
+      Mushroom m = await _db.Mushrooms.FindAsync(id);
+      if (m == null)
+      {
+        return NotFound();
+      }
+      _db.Mushrooms.Remove(m);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+
+    }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Mushroom m)
+    {
+      if (id != m.MushroomId)
+      {
+        return BadRequest();
+      }
+      _db.Mushrooms.Update(m);
+      
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!MushroomExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+    private bool MushroomExists(int id)
+    {
+      return _db.Mushrooms.Any(e => e.MushroomId == id);
+    }
+
 
   }
 
